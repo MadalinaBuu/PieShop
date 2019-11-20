f8fa62b3d6661b4343c5e2dae9a9e3a0e45bbdb1
Mădălina <madalina.ciucioiu@gmail.com>
2019-11-20 18:06:57 +0000

    Adding ViewComponent - CategoryMenu

--- PieShop/PieShop/Controllers/PieController.cs
+++ PieShop/PieShop/Controllers/PieController.cs
@@ -12,31 +12,44 @@ namespace PieShop.Controllers
 {
     public class PieController : Controller
     {
-        private readonly IPieRepository _pieRespository;
-        private readonly ICategoryRepository _categoryRespository;
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
 
-        public PieController(IPieRepository pieRespository, ICategoryRepository categoryRespository)
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
         {
-            _pieRespository = pieRespository;
-            _categoryRespository = categoryRespository;
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+        public ViewResult List(string category)
+        {
+            IEnumerable<Pie> pies;
+            string currentCategory = string.Empty;
+
+            if (string.IsNullOrEmpty(category))
+            {
+                pies = _pieRepository.AllPies.OrderBy(p => p.PieId);
+                currentCategory = "All pies";
+            }
+            else
+            {
+                pies = _pieRepository.AllPies.Where(p => p.Category.CategoryName == category)
+                   .OrderBy(p => p.PieId);
+                currentCategory = _categoryRepository.AllCategories.FirstOrDefault(c => c.CategoryName == category).CategoryName;
+            }
+
+            return View(new PiesListViewModel
+            {
+                Pies = pies,
+                CurrentCategory = currentCategory
+            });
         }
 
-        public ViewResult List()
-        {
-            PiesListViewModel piesListViewModel = new PiesListViewModel();
-            piesListViewModel.Pies = _pieRespository.AllPies;
-
-            piesListViewModel.CurrentCategory = "Cheese cakes";
-
-            return View(piesListViewModel);
-        }
-
-        public IActionResult Details(int id)
-        {
-            var pie = _pieRespository.GetPieById(id);
-            if (pie == null)
-                return NotFound();
-            return View(pie);
-        }
+            public IActionResult Details(int id)
+            {
+                var pie = _pieRepository.GetPieById(id);
+                if (pie == null)
+                    return NotFound();
+                return View(pie);
+            }
     }
 }
